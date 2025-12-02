cda6059ff35f488d71c91994e696e082be584919
RYANDEXT <ryandwikidermawan06@gmail.com>
2025-12-02 16:36:30 +0700

    responsive mobile layout

--- components/contact/ContactSection.tsx
+++ components/contact/ContactSection.tsx
@@ -1,33 +1,34 @@
 import ContactForm from "./emails/ContactForm";
 
 export default function ContactSection() {
-  return (
-    <section id="contact-section">
-      <div className="max-w-5xl mx-auto space-y-8 md:py-28 pb-14">
-        <div className="text-left space-y-4">
-          <div>
-            <p className="mt-8 font-medium text-lime-300 text-lg">
-              Connect with me
-            </p>
-            <h2 className="text-5xl">Contact</h2>
-          </div>
-        </div>
-        <div className="flex">
-          <div className="flex-1 mr-4">
-            <h1 className="font-semibold text-2xl mb-5 text-lime-300">
-              Let&apos;s build something great together! 🚀
-            </h1>
-            <p>
-              Whether it’s a quick question about my stack, a challenging
-              project idea, or just an interest in connecting with a fellow
-              developer, don&apos;t hesitate to drop me a line!
-            </p>
-          </div>
-          <div className="flex-1 bg-black/30 p-8 rounded-xl border-gray-50/10 border mr-4">
-            <ContactForm />
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+    return (
+        <section id="contact-section">
+            <div className="max-w-5xl mx-auto px-4 md:px-0 space-y-8 md:py-28 pb-14">
+                <div className="text-center md:text-left space-y-4">
+                    <div>
+                        <p className="mt-8 font-medium text-lime-300 text-lg">
+                            Connect with me
+                        </p>
+                        <h2 className="text-5xl">Contact</h2>
+                    </div>
+                </div>
+                <div className="flex flex-col md:flex-row gap-6 items-center md:items-start">
+                    <div className="w-full md:flex-1 md:mr-4 text-center md:text-left">
+                        <h1 className="font-semibold text-2xl mb-5 text-lime-300">
+                            Let&apos;s build something great together! 🚀
+                        </h1>
+                        <p>
+                            Whether it’s a quick question about my stack, a
+                            challenging project idea, or just an interest in
+                            connecting with a fellow developer, don&apos;t
+                            hesitate to drop me a line!
+                        </p>
+                    </div>
+                    <div className="w-full md:flex-1 bg-black/30 p-6 md:p-8 rounded-xl border-gray-50/10 border">
+                        <ContactForm />
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 }
--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -2,54 +2,54 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Footer() {
-  return (
-    <div className=" flex justify-between items-center max-w-5xl my-7 mx-auto">
-      <div className="text-slate-500">
-        © 2025 Ryan Darmawan. All rights reserved
-      </div>
-      <div className="max-w-50% flex justify-around gap-3">
-        <Link
-          href="https://www.linkedin.com/in/ryan-dwiky-darmawan-7865a4197/"
-          target="_blank"
-        >
-          <Image
-            src="/icons/footer/linkedin-svgrepo-com.svg"
-            alt="linkedin"
-            width={20}
-            height={20}
-          ></Image>
-        </Link>
+    return (
+        <div className="flex flex-col md:flex-row justify-between items-center max-w-5xl my-7 mx-auto gap-3 px-4">
+            <div className="text-slate-500 text-center md:text-left w-full md:w-auto">
+                © 2025 Ryan Darmawan. All rights reserved
+            </div>
+            <div className="flex gap-3 items-center">
+                <Link
+                    href="https://www.linkedin.com/in/ryan-dwiky-darmawan-7865a4197/"
+                    target="_blank"
+                >
+                    <Image
+                        src="/icons/footer/linkedin-svgrepo-com.svg"
+                        alt="linkedin"
+                        width={20}
+                        height={20}
+                    ></Image>
+                </Link>
 
-        <Link href="https://github.com/RYANDEXT" target="_blank">
-          <Image
-            src="/icons/footer/github-svgrepo-com.svg"
-            alt="linkedin"
-            width={20}
-            height={20}
-          ></Image>
-        </Link>
+                <Link href="https://github.com/RYANDEXT" target="_blank">
+                    <Image
+                        src="/icons/footer/github-svgrepo-com.svg"
+                        alt="linkedin"
+                        width={20}
+                        height={20}
+                    ></Image>
+                </Link>
 
-        <Link
-          href="https://mail.google.com/mail/u/0/?fs=1&to=ryandwikidermawan06@gmail.com&su=Hey+Ryan!&tf=cm"
-          target="_blank"
-        >
-          <Image
-            src="/icons/footer/email-svgrepo-com.svg"
-            alt="linkedin"
-            width={20}
-            height={20}
-          ></Image>
-        </Link>
+                <Link
+                    href="https://mail.google.com/mail/u/0/?fs=1&to=ryandwikidermawan06@gmail.com&su=Hey+Ryan!&tf=cm"
+                    target="_blank"
+                >
+                    <Image
+                        src="/icons/footer/email-svgrepo-com.svg"
+                        alt="linkedin"
+                        width={20}
+                        height={20}
+                    ></Image>
+                </Link>
 
-        <Link href="https://www.instagram.com/ryand.d/" target="_blank">
-          <Image
-            src="/icons/footer/instagram-svgrepo-com.svg"
-            alt="linkedin"
-            width={20}
-            height={20}
-          ></Image>
-        </Link>
-      </div>
-    </div>
-  );
+                <Link href="https://www.instagram.com/ryand.d/" target="_blank">
+                    <Image
+                        src="/icons/footer/instagram-svgrepo-com.svg"
+                        alt="linkedin"
+                        width={20}
+                        height={20}
+                    ></Image>
+                </Link>
+            </div>
+        </div>
+    );
 }
