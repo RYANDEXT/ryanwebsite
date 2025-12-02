2190ad2cb83ab060f6d7d20325bf54983877b213
RYANDEXT <ryandwikidermawan06@gmail.com>
2025-12-02 16:47:34 +0700

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,14 @@ import ContactSection from "@/components/contact/ContactSection";
 import Footer from "@/components/footer/Footer";
 
 export default function Home() {
-  return (
-    <>
-      <HomeSection />
-      <div className=" h-1 bg-gray-600/50 max-w-5xl mx-auto rounded-lg"></div>
-      <WorkSection />
-      <div className=" h-1 bg-gray-600/50 max-w-5xl mx-auto rounded-lg"></div>
-      <ContactSection />
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <HomeSection />
+            <div className=" h-1 bg-gray-600/50 max-w-5xl mx-auto rounded-lg"></div>
+            <WorkSection />
+            <div className=" h-1 bg-gray-600/50 max-w-5xl mx-auto rounded-lg"></div>
+            <ContactSection />
+            <Footer />
+        </>
+    );
 }
